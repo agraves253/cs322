a494e15e28f36afc4938c10e24a83f943c30d7d9
agraves253 <131315645+agraves253@users.noreply.github.com>
2023-05-01 06:18:35 -0700

    Update SI.cpp

--- SI.cpp
+++ SI.cpp
@@ -1,86 +1,75 @@
 #include <ncurses.h>
-#include <thread>
 #include <unistd.h>
-#define DELAY 10000
+
+#define DELAY 60000
 
 class Alien
 {
-
-int x, y;
-int max_x;
-int direction;
-
 public:
-        Alien(int initialX, int initialY, int maxX, int direction)
-        :x(initialX), y(initialY), max_x(maxX), direction(direction){}
+    int x, y;
+    int max_x;
+    int direction;
 
-        void draw()
+    Alien(int initialX, int initialY, int maxX, int direction)
+    :x(initialX), y(initialY), max_x(maxX), direction(direction){}
+
+    void draw()
+    {
+        mvaddstr(y-1, x, "   | ");
+        mvaddstr(y, x, " _/\\_");
+        mvaddstr(y+1, x , "/____\\");
+    }
+
+    void move()
+    {
+        int next_x = x + direction;
+
+        if(next_x+10 >= max_x || next_x < 0 )
         {
-                mvaddstr(y-1, x, "  | ");
-                mvaddstr(y, x, "_/\\_");
-                mvaddstr(y+1, x, "/___\\");
-                refresh();
+            direction*= -1;
         }
-
-        void move()
+        else
         {
-                int next_x = x + direction;
-
-                if(next_x+10 >= max_x || max_x < 0)
-                {
-                        direction *= -1;
-                }
-                else
-                {
-                        x+=direction;
-                }
+            x+=direction;
         }
+    }
 };
 
 int main(int argc, char *argv[])
 {
-        //initializes ncurses
-        initscr();
-        noecho();
-        curs_set(FALSE);
+    initscr();
+    noecho();
+    curs_set(FALSE);
 
-        //get dimensions of screen
-        int max_y, max_x;
+    // Global var 'stdscr is created by the call to 'initscr()'
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
 
-        //getmaxyx(stdscr, max_y, max_x);
+    int num_aliens = 10;
+    Alien aliens[num_aliens];
 
-        //populate aliens
-        int number_aliens = 10;
+    for(int i=0; i<num_aliens; i++)
+    {
+        aliens[i] = Alien(i*20, 0, max_x, 1);
+    }
 
-        Alien aliens[] =
+    while(1)
+    {
+        clear();
+
+        for(int i=0; i<num_aliens; i++)
         {
-                Alien(0,0,max_x,1),
-                Alien(20,0,max_x,-1),
-                Alien(40,0,max_x,1),
-        };
-
-        while(1)
-        {
-                clear();
-
-                getmaxyx(stdscr, max_y, max_x);
-
-                //draw aliens
-                for(int i = 0; i < number_aliens; i++)
-                {
-                        aliens[i].draw();
-                }
-                refresh();
-                usleep(DELAY);
-                //refresh alien position
-                for(int i = 0; i < number_aliens; i++)
-                {
-                        aliens[i].move();
-                }
-
-
-
+            aliens[i].draw();
         }
 
-        endwin();
+        refresh();
+        usleep(DELAY);
+
+        for(int i=0; i<num_aliens; i++)
+        {
+            aliens[i].move();
+        }
+    }
+
+    endwin();
 }
