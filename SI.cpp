f86541f65ec9e24a86df2946c824c157def77314
agraves253 <131315645+agraves253@users.noreply.github.com>
2023-05-01 06:52:57 -0700

    Update SI.cpp

--- SI.cpp
+++ SI.cpp
@@ -19,21 +19,22 @@ public:
     mvaddstr(y + 1, x, "/____\\");
   }
 
-  void move(std::array<Alien, 5>& aliens) {
-    int next_x = x + direction;
+  void move()
+  {
+      int next_x = x + direction;
 
-    if (next_x + 10 >= max_x || next_x < 0) {
-      // Change the direction of each alien in the array
-      for (int i = 0; i < aliens.size(); i++) {
-        aliens[i].direction *= -1;
+      if (next_x + 10 >= max_x || next_x < 0)
+      {
+          // Change the direction of each alien in the array
+          for (int i = 0; i < num_aliens; i++)
+          {
+              aliens[i].direction *= -1;
+          }
       }
-      // Move each alien in the opposite direction after changing direction
-      for (int i = 0; i < aliens.size(); i++) {
-        aliens[i].x += aliens[i].direction;
+      else
+      {
+          x += direction;
       }
-    } else {
-      x += direction;
-    }
   }
 };
 
@@ -46,12 +47,17 @@ int main(int argc, char *argv[]) {
   int max_y, max_x;
   getmaxyx(stdscr, max_y, max_x);
 
-  std::array<Alien, 5> aliens = {{
-      Alien(0 * 20, 0, max_x, 1),
-      Alien(1 * 20, 0, max_x, 1),
-      Alien(2 * 20, 0, max_x, 1),
-      Alien(3 * 20, 0, max_x, 1),
-      Alien(4 * 20, 0, max_x, 1),
+  std::array<Alien, 20> aliens = {{
+      Alien(0 * 20, 0, max_x, 1), Alien(0 * 20, 3 * 20, max_x, 1),
+      Alien(1 * 20, 0, max_x, 1), Alien(1 * 20, 3 * 20, max_x, 1),
+      Alien(2 * 20, 0, max_x, 1), Alien(2 * 20, 3 * 20, max_x, 1),
+      Alien(3 * 20, 0, max_x, 1), Alien(3 * 20, 3 * 20, max_x, 1),
+      Alien(4 * 20, 0, max_x, 1), Alien(4 * 20, 3 * 20, max_x, 1),
+      Alien(5 * 20, 0, max_x, 1), Alien(5 * 20, 3 * 20, max_x, 1),
+      Alien(6 * 20, 0, max_x, 1), Alien(6 * 20, 3 * 20, max_x, 1),
+      Alien(7 * 20, 0, max_x, 1), Alien(7 * 20, 3 * 20, max_x, 1),
+      Alien(8 * 20, 0, max_x, 1), Alien(8 * 20, 3 * 20, max_x, 1),
+      Alien(9 * 20, 0, max_x, 1), Alien(9 * 20, 3 * 20, max_x, 1)
   }};
 
   while (1) {
@@ -65,7 +71,7 @@ int main(int argc, char *argv[]) {
     usleep(DELAY);
 
     for (int i = 0; i < aliens.size(); i++) {
-      aliens[i].move(aliens);
+      aliens[i].move();
     }
   }
 
