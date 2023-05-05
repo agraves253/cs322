f86e33a3036367e647ba95f0c4dcf70bd962cc59
agraves253 <131315645+agraves253@users.noreply.github.com>
2023-05-04 21:18:55 -0700

    Update alien.cpp

--- alien.cpp
+++ alien.cpp
@@ -1,33 +1,52 @@
 #include <ncurses.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <string>
 
+//Alien class
 class Alien 
 {
 public:
-    Alien(int x, int y, char symbol)
+    //create an Alien object
+    Alien(int x, int y, std::string symbol)
         : x_(x), y_(y), symbol_(symbol), direction_(1) {}
 
+    // Move the alien horizontally by adding the direction to its x-coordinate
     void move() 
     {
         x_ += direction_;
     }
 
+    // Reverse the direction of the alien and move it down one row
     void reverseDirection() 
     {
         direction_ *= -1;
         y_ += 1;
     }
 
-    void draw() const {
-        mvaddch(y_, x_, symbol_);
+    // Draw the alien on the screen
+    void draw() const 
+    {
+        for (int i = 0; i < symbol_.size(); ++i) 
+        {
+            mvaddch(y_, x_ + i, symbol_[i]);
+        }
     }
 
+    // Return the x-coordinate of the alien
     int x() const { return x_; }
+
+    // Return the y-coordinate of the alien
     int y() const { return y_; }
-    char symbol() const { return symbol_; }
+
+    // Return the symbol of the alien
+    std::string symbol() const { return symbol_; }
 
 private:
     int x_;
     int y_;
-    char symbol_;
+    std::string symbol_;
     int direction_;
 };
