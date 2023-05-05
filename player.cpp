edc3745cd54cc086df5947127b7f59acaae7ad9c
agraves253 <131315645+agraves253@users.noreply.github.com>
2023-05-04 21:20:17 -0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,27 +1,53 @@
 #include <ncurses.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <string>
 
+//player class
 class Player 
 {
 public:
-    Player(int x, int y, char symbol)
+    //initialize player's starting position and symbol
+    Player(int x, int y, const std::string& symbol)
         : x_(x), y_(y), symbol_(symbol) {}
 
+    //move the player's position by dx and dy
     void move(int dx, int dy) 
     {
         x_ += dx;
         y_ += dy;
+        // Ensure the player stays within the screen boundaries
+        if (x_ < 0) 
+        {
+            x_ = 0;
+        }
+        if (x_ + symbol_.length() > COLS) 
+        {
+            x_ = COLS - symbol_.length();
+        }
     }
 
-    void draw() const {
-        mvaddch(y_, x_, symbol_);
+    //draw the player at their current position
+    void draw() const {       
+        for (size_t i = 0; i < symbol_.length(); ++i) 
+        {
+            mvaddch(y_, x_ + i, symbol_[i]);
+        }
     }
 
+    //return the player's x coordinate
     int x() const { return x_; }
+
+    //return the player's y coordinate
     int y() const { return y_; }
-    char symbol() const { return symbol_; }
+
+    //return player's symbol
+    const std::string& symbol() const { return symbol_; }
 
 private:
-    int x_;
-    int y_;
-    char symbol_;
+    int x_; 
+    int y_; 
+    std::string symbol_; 
 };
